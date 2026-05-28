8fa23fa0fe386c0486287f5df532162bd786461e
lucasnavarrodufur <lucasnavarrodufur@gmail.com>
2026-05-28 20:24:05 +0000

    Test Subir Sample - MIME Type Inválido + Arreglo de Test Subir Sample (Simulado) ahora con una cabecera valida

--- frontend/js/tests/sampleTests.js
+++ frontend/js/tests/sampleTests.js
@@ -47,9 +47,21 @@ testUtils.createTestButton("Test Subir Sample (Simulado)", async (btn) => {
     formData.append('category', 'Drums');
     formData.append('bpm', '120');
 
-    // Simulamos un archivo WAV (binario vacío para la prueba)
-    const blob = new Blob(["Simulated Audio Content"], { type: 'audio/wav' });
-    formData.append('audioFile', blob, 'DRUM_LOOP_01.wav');
+    //Simulamos un cabecera válida
+    const wavHeader = new Uint8Array([
+        0x52, 0x49, 0x46, 0x46, // "RIFF" en hex
+        0x00, 0x00, 0x00, 0x00, // Tamaño dummy (4 bytes)
+        0x57, 0x41, 0x56, 0x45, // "WAVE" en hex
+        0x66, 0x6D, 0x74, 0x20  // "fmt " en hex
+    ]);
+    
+    //Usamos TextEncoder para convertir el string a bytes válidos
+    const dummyContent = new TextEncoder().encode("Simulated Audio Content");
+    
+    // Simulamos un archivo WAV (Header+Contenido)
+    const validBlob = new Blob([wavHeader, dummyContent], { type: 'audio/wav' });
+    
+    formData.append('audioFile', validBlob, 'DRUM_LOOP_01.wav');
 
     const response = await fetch('/api/samples/upload', {
         method: 'POST',
@@ -96,4 +108,41 @@ testUtils.createTestButton("Test Subir Sample Demasiado Grande (Simulado)", asyn
     testUtils.log(data);
 
     if (response.status === 413) testUtils.setSuccess(btn);
+});
+
+/**
+ * Test: POST /api/samples/upload (MIME Type Inválido)
+ */
+testUtils.createTestButton("Test Subir Sample - MIME Type Inválido", async (btn) => {
+    // 1. Asegurar y guardar una sesión válida
+    await okLogin();
+    const token = localStorage.getItem('test_token');
+    
+    // Creamos un FormData con los campos requeridos
+    const formData = new FormData();
+    formData.append('display_name', 'Test_MIME_Falso');
+    formData.append('category', 'test');
+    formData.append('bpm', '120');
+
+    // Simulamos un archivo que renombra un .txt a .wav para evadir validaciones
+    const fakeContent = 'Este es texto plano disfrazado de archivo de audio';
+    const fakeFile = new Blob([fakeContent], { type: 'audio/wav' });
+    
+    formData.append('audioFile', fakeFile, 'prueba_falsa.wav');
+
+    const response = await fetch('/api/samples/upload', {
+        method: 'POST',
+        headers: { 'Authorization': `Bearer ${token}` },
+        body: formData
+    });
+
+    const data = await response.json();
+    testUtils.log(data);
+
+    const esStatus415 = response.status === 415;
+    const mensajeCorrecto = data.error === 'El archivo no es un audio válido';
+    
+    if (esStatus415 && mensajeCorrecto) {
+        testUtils.setSuccess(btn);
+    }
 });
