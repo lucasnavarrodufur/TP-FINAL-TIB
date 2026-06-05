90ac0586a0ccf81542a06c0ff4958cf7c2413f89
Martín Iriberri <iriberri@alumnos.fi.mdp.edu.ar>
2026-06-05 02:46:01 +0000

    Limipeza manual de sesión en handler global por motivos de seguridad

--- frontend/js/components/globalHandlers.js
+++ frontend/js/components/globalHandlers.js
@@ -8,7 +8,14 @@ window.addEventListener('unauthenticated', () => {
     /**
      * En este caso, para mantener authHelper en su estado original y que él sea el encargado de limpiar el local storage y redirigir al login,
      * se muestra el modal y, cuando se cierra, se ejecuta authHelper.logout()
+     * 
+     * ACTUALIZACIÓN: Se limpia localStorage manualmente antes de llamar a logout y así no depender de que el usuario presione el botón para
+     * eliminar la sesión actual. Luego se muestra el modal y, cuando se cierra, se ejecuta logout para forzar la redirección al login.
+     * Redundante pero seguro.
      */
+
+    // Limpiar sesión actual manualmente
+    localStorage.clear();
 
     // Mostrar modal de sesión expirada
     showModal('Error de autenticación', 'Sesión inválida o corrompida');
